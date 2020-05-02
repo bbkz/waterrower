843569d9da9f7d09e1194c23e0d81214b3cf3641
bbk <bbk@nocloud.ch>
2020-05-02 12:31:21 +0200

    unbeautify polar chart code / assimilate code

--- waterrower/static/graph.js
+++ waterrower/static/graph.js
@@ -1,62 +1,90 @@
 var polarchart = (function() {
-  var svg = null;
-  h = parseInt(d3.select("#polarchart").style('height'), 10);
-  w = parseInt(d3.select("#polarchart").style('width'), 10);
-
-  var r = Math.min(w,h) / 1;
-  var s = 0.09;
-
+  var polarsvg = null;
+  var h = parseInt(d3.select("#polarchart").style('height'), 10);
+  var w = parseInt(d3.select("#polarchart").style('width'), 10);
   var startingData = [{name: 'm', index: 0.1, value: 0},
                       {name: 'm/s', index: 0.2, value: 0},
                       {name: 's/m', index: 0.3, value: 0}];
 
+  var renderPolarChart = function(selection, arrData) {
+    var r = Math.min(w, h) / 1;
+    var s = 0.09;
 
-  var fill = d3.scaleLinear()
-    .range(["#f00", "#0f0", "#ff0"]);
-  var arc = d3.arc()
-    .startAngle(0)
-    .endAngle(function(d) { return d.value * 2 * Math.PI; })
-    .innerRadius(function(d) { return d.index * r; })
-    .outerRadius(function(d) { return (d.index + s) * r; });
+    var arcValue = d => d.value;
+    var arcIndex = d => d.index;
+    var arcName  = d => d.name;
+
+    var fill = d3.scaleLinear()
+      .range(['#d73027', '#1a9850'])
+      .interpolate(d3.interpolateRgb); //interpolateHsl interpolateHcl interpolateRgb
+
+    var genEndAngle = d => arcValue(d) * 2 * Math.PI;
+    var genInnerRad = d => arcIndex(d) * r;
+    var genOuterRad = d => (arcIndex(d) + s) * r;
+
+    var arcGenerator = d3.arc()
+      .startAngle(0)
+      .endAngle(d => genEndAngle(d))
+      .innerRadius(d => genInnerRad(d))
+      .outerRadius(d => genOuterRad(d));
+
+    // add data to the arcs
+    //
+    // selection filled with d data
+    var allArcG = selection.selectAll(".arc")
+      .data(arrData);
+
+    // update data
+    allArcG.select("path")
+      .attr("d", d => arcGenerator(d))
+      .style("fill", d => fill(arcValue(d)));
+
+    // on enter new data
+    allArcG.select("path")
+      .enter()
+      .attr("d", d => arcGenerator(d));
+
+    // handle removed data
+    allArcG.select("path")
+      .exit()
+      .remove();
+
+    allArcG.select("text")
+      .attr("dy", d => arcValue(d) < .5 ? "-.5em" : "1em")
+      .attr("transform", d => "rotate(" + 360 * arcValue(d) + ")"
+        + "translate(0," + -(arcIndex(d) + s / 2) * r + ")"
+        + "rotate(" + (arcValue(d) < .5 ? -90 : 90) + ")")
+      .text(d => arcName(d));
+  }
 
   var init = function() {
-    svg = d3.select("#polarchart").append("svg")
-      .attr("width", w)
-      .attr("height", h)
-      .append("g")
-      .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
+    polarsvg = d3.select("#polarchart").append("svg")
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .attr('viewBox','0 0 '+Math.min(w, h)+' '+Math.min(w, h))
+      .attr('preserveAspectRatio','xMinYMin');
 
-    var g = svg.selectAll("g")
-      .data(startingData, function(d) { return d.index; })
+    var polarG = polarsvg.append("g")
+      .attr("class", "arcgroup")
+      .attr("transform", "translate(" + Math.min(w, h) / 2 + "," + Math.min(w, h) / 2 + ")");
+
+    // for each row in data append a g
+    pallG = polarG.selectAll(".arcgroup")
+      .data(startingData, d => d.index)
       .enter()
-      .append("g");
-
-    g.append("path")
-      .style("fill", function(d) { return fill(d.value); })
-      .attr("d", arc);
-
-    g.append("text")
+        .append("g")
+          .attr("class", "arc");
+    pallG.append("path")
+      .attr("class", "arcpath");
+    pallG.append("text")
       .attr("text-anchor", "middle")
       .attr("dy", "1em")
-      .text(function(d) { return d.name; });
+
+    update(startingData);
   }
 
   var update = function(data) {
-    var g = svg.selectAll("g")
-      .data(data);
-
-    g.select("path")
-      .style("fill", function(d) { return fill(d.value); })
-      .attr("d", arc);
-
-    g.select("text")
-      .attr("dy", function(d) { return d.value < .5 ? "-.5em" : "1em"; })
-      .attr("transform", function(d) {
-        return "rotate(" + 360 * d.value + ")"
-          + "translate(0," + -(d.index + s / 2) * r + ")"
-          + "rotate(" + (d.value < .5 ? -90 : 90) + ")"
-      })
-      .text(function(d) { return d.name; });
+    renderPolarChart(polarsvg, data)
   }
 
   var reset = function() {
@@ -72,12 +100,12 @@ var polarchart = (function() {
 })();
 
 var linechart = (function() {
-  var svg = null
+  var linesvg = null
   var startingData = [
     {stroke_rate: 0, heart_rate: 0, total_distance_m: 0, total_strokes: 0, avg_distance_cmps: 0, time: 0, elapsed: 0}
   ];
 
-  var renderChart = function(selection, arrData) {
+  var renderLineChart = function(selection, arrData) {
     margin = ({top: 20, right: 30, bottom: 20, left: 30})
     h = parseInt(selection.style('height'), 10);
     w = parseInt(selection.style('width'), 10);
@@ -100,10 +128,10 @@ var linechart = (function() {
     //
     // create axisgenerators
     xAxis = g => g
-      .attr("transform", `translate(0,${h - margin.bottom})`)
+      .attr("transform", `translate(0, ${h - margin.bottom})`)
       .call(d3.axisBottom(xScale));
     yAxis1 = g => g
-      .attr("transform", `translate(${margin.left},0)`)
+      .attr("transform", `translate(${margin.left}, 0)`)
       .call(d3.axisLeft(yScale1));
     yAxis2 = g => g
       .attr("transform", `translate(${w - margin.right}, 0)`)
@@ -132,17 +160,17 @@ var linechart = (function() {
     // add data to the lines
     //
     // line selection
-    line1 = selection.selectAll(".line-path1");
-    line2 = selection.selectAll(".line-path2");
+    line1 = selection.selectAll(".linepath1");
+    line2 = selection.selectAll(".linepath2");
 
     // line update data
     line1.attr("d", d => lineGenerator1(arrData));
     line2.attr("d", d => lineGenerator2(arrData));
 
     // line on enter new data
-    line1.enter().selectAll("line-path1")
+    line1.enter().selectAll(".linepath1")
       .attr("d", d => lineGenerator1(arrData));
-    line2.enter().selectAll("line-path2")
+    line2.enter().selectAll(".linepath2")
       .attr("d", d => lineGenerator2(arrData));
 
     // line handle removed data
@@ -152,29 +180,27 @@ var linechart = (function() {
 
   var init = function() {
     // create containers
-    svg = d3.select('#linechart').append("svg")
+    linesvg = d3.select('#linechart').append("svg")
       .attr("height", "100%")
       .attr("width", "100%");
-    svg.append("g")
+    linesvg.append("g")
       .attr("class", "xaxis");
-    svg.append("g")
+    linesvg.append("g")
       .attr("class", "y1axis");
-    svg.append("g")
+    linesvg.append("g")
       .attr("class", "y2axis");
-    svg.append("path")
-      .attr("class", "line-path1")
+    lineG = linesvg.append("g")
+      .attr("class", "linepaths")
       .attr("fill", "none")
+      .attr("stroke-width", 5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round");
+    lineG.append("path")
+      .attr("class", "linepath1")
       .attr("stroke", "steelblue")
-      .attr("stroke-width", 5)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round");
-    svg.append("path")
-      .attr("class", "line-path2")
-      .attr("fill", "none")
+    lineG.append("path")
+      .attr("class", "linepath2")
       .attr("stroke", "orange")
-      .attr("stroke-width", 5)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round");
   }
 
   var update = function(data) {
@@ -185,7 +211,7 @@ var linechart = (function() {
       // return item.time >= range && item.stroke_rate != 0 && item.heart_rate != 0;
       return item.time >= range && item.stroke_rate != 0;
     });
-    renderChart(svg, data)
+    renderLineChart(linesvg, data)
   }
 
   var reset = function() {
